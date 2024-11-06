4c6cedc8c90e842dc947289caee1743c9aaff326
Mubashar Hussain <codewithmubasharhussain@gmail.com>
2024-11-06 14:29:50 +0500

    creating website layout

--- game-website/src/App.jsx
+++ game-website/src/App.jsx
@@ -1,11 +1,21 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem ,Show} from '@chakra-ui/react'
 
 function App() {
 
   return (
     <>
-     <Button>hi</Button>
-     <Button>Hello</Button>
+      <Grid templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav""aside main"`
+
+      }}>
+        <GridItem area={"nav"} bg={"coral"}>Nav</GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"gold"}>aside</GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"dodgerblue"}>main</GridItem>
+
+      </Grid>
     </>
   )
 }
