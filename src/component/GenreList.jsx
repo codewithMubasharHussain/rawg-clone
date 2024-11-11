f238d260b6c95b29006bd3cdab0f3e8f23686fa8
Mubashar Hussain <codewithmubasharhussain@gmail.com>
2024-11-11 11:51:05 +0500

    adding project pic

--- src/component/GenreList.jsx
+++ src/component/GenreList.jsx
@@ -1,12 +1,18 @@
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenre from "../Hooks/useGenre";
 
 function GenreList(){
-    const {Genres} = useGenre();
+    const {Genres,isloading} = useGenre();
+    if(isloading) return <Spinner/>
     return(
         <>
-        <ul>
-            {Genres.map(genre => <li>{genre.name}</li>)}
-        </ul>
+        <List>
+            {Genres.map(genre => <ListItem paddingY={"5px"} key={genre.id}><HStack>
+                <Image boxSize={"32px"} borderRadius={8} src={genre.image_background}/>
+                <Text fontSize={"lg"}>{genre.name}</Text>
+                </HStack>
+                </ListItem>)}
+        </List>
         </>
     )
 }
